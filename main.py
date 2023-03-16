aebf3ad65489b8611be823a8a353d735c63b39b9
viktor <mlviktor23@gmail.com>
2023-03-16 23:20:52 +0100

    Lab 1

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ import networkx as nx
 # unix mailbox recipe
 # see https://docs.python.org/3/library/mailbox.html
 
-
 def mbox_graph():
     mbox = mailbox.mbox("enron.baughman-d.power.legal_agreements")  # parse unix mailbox
 
-    G = nx.MultiDiGraph()  # create empty graph
+    G = nx.Graph()  # create empty graph
 
     # parse each messages and build graph
     for msg in mbox:  # msg is python email.Message.Message object
@@ -33,12 +32,47 @@ def mbox_graph():
 
 G = mbox_graph()
 
+# Remove self-loops
+G.remove_edges_from(nx.selfloop_edges(G))
+
 # print edges with message subject
 for (u, v, d) in G.edges(data=True):
     print(f"From: {u} To: {v} Subject: {d['message']['Subject']}")
 
 pos = nx.spring_layout(G)
-nx.draw(G, pos, node_size=4, alpha=0.1, edge_color="g")
+sizes = [v * 10 for v in dict(G.degree()).values()]
+nx.draw(G, pos, node_size=sizes, edge_color="grey", alpha=0.5)
 ax = plt.gca()
 ax.margins(0.08)
 plt.show()
+
+# plot node degrees
+plt.figure(figsize=(10,6))
+plt.subplots_adjust(bottom=0.2)
+degree_sequence = sorted([(n, d) for n, d in G.degree()], key=lambda x: x[1], reverse=True)
+top_nodes = [n for n, d in degree_sequence[:5]]
+x_ticks = top_nodes
+degree_sequence = [d for n, d in degree_sequence if n in top_nodes]
+plt.bar(x_ticks, degree_sequence)
+plt.ylabel('Degree')
+plt.title('Degree distribution of 5 nodes with highest degree')
+plt.xticks(rotation=20, fontsize=10)
+plt.show()
+
+# calculate the degree histogram and plot it
+dh = nx.degree_histogram(G)
+dgs = list(range(0,len(dh)))
+plt.bar(dgs, dh)
+plt.xlabel('Degree')
+plt.ylabel('Number of Nodes')
+plt.title('Degree distribution of graph')
+plt.show()
+
+# loglog plot of the degree frequencies
+plt.loglog(dgs,dh)
+plt.title("Loglog plot of degree frequencies")
+plt.show()
+
+print("\nThe graph has a clustering coefficient of", nx.average_clustering(G),
+      "an average shortest path length of", nx.average_shortest_path_length(G),
+      "and a density of", nx.density(G))
